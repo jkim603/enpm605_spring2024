3f28918737e30f0b7969db4873a8c2e65a54ecb1
jkim603 <113157667+jkim603@users.noreply.github.com>
2024-02-28 13:27:21 -0500

    Added comments and docstrings

--- rwa2_kim/main.py
+++ rwa2_kim/main.py
@@ -8,51 +8,67 @@ from maze import ROBOT, EMPTY, OBSTACLE, BOOM, GOAL
 from maze import robot_position, robot_orientation, goal_position, maze
 from robot import update_robot
 
-# def obstacle_goal_check():
-#     global robot_position, OBSTACLE, BOOM, GOAL
-#     if maze[robot_position[0]][robot_position[1]] == OBSTACLE:
-#         maze[robot_position[0]][robot_position[1]] = BOOM
-#         print("Oh no! You hit an obstacle and the robot exploded!")
-#         return True
-#     elif maze[robot_position[0]][robot_position[1]] == GOAL:
-#         print("Goal reached! Congratulations! You won!!!")
-#         return True
-#     else:
-#         return False
-
 def main():
+    '''
+    This program runs the robot maze game. It queries the user for action inputs and updates and displays the maze accordingly.
+    '''
+    # print the initial maze defined in maze.py
     print("Initial Maze:")
     print_maze()
+    
+    # open loop to query user for action inputs
     while True:
+        # define actions and get input
         action = input("Enter action (w: forward, s: backward, a: left, d: right, q: quit): ")
-        # TODO write the main function
+        # check if action is within valid options
         if action not in ["w","a","s","d","q"]:
+            # tell user they have given invalid input, ask to try again
             print("\nInvalid action given, please try again.\n")
+            # restart input query loop
             continue
+        # check if action is quit, then display a message and break the loop to end program
         elif action == "q":
             print("\nQuitting the maze game. Have a nice day!\n")
             break
-        global ROBOT, EMPTY, robot_position, robot_orientation, maze
+        
+        # use global variables to update robot position and orientation
+        global EMPTY, robot_position, robot_orientation, maze
+        # set old robot position to display as an empty spot on maze
         maze[robot_position[0]][robot_position[1]] = EMPTY
-        ROBOT, robot_position, robot_orientation = update_robot(action, ROBOT, robot_position, robot_orientation)
+        # get new robot position and orientation depending on action
+        ROBOT, robot_position, robot_orientation = update_robot(action, robot_position, robot_orientation)
+        
+        # check if new robot position hits an obstacle
         if maze[robot_position[0]][robot_position[1]] is OBSTACLE:
+            # if robot hits obstacle, update maze to show explosion
             maze[robot_position[0]][robot_position[1]] = BOOM
-            print("\nOh no! You hit an obstacle and the robot exploded!")
+            # tell user they have hit an obstacle and it is game over
+            print("\nOh no! You hit an obstacle and the robot exploded! Better luck next time...")
+            # create boolean that will be used to end program after printing maze
             end_check = True
+        # check if new robot position is the goal
         elif maze[robot_position[0]][robot_position[1]] is GOAL:
+            # if robot reaches goal, update maze to show the robot where the flag used to be
             maze[robot_position[0]][robot_position[1]] = ROBOT
+            # tell user they have won the game
             print("\nGoal reached! Congratulations! You won!!!")
+            # create boolean that will be used to end program after maze is printed
             end_check = True
+        # if robot did not hit obstacle or reach goal, update maze to show new robot position and orientation
         else:
+            # show robot in its new position and orientation in maze
             maze[robot_position[0]][robot_position[1]] = ROBOT
+            # create boolean that will be used to continue program after maze is printed
             end_check = False
-        # stop_check = obstacle_goal_check()
-        # if stop_check is True:
-            # break
-        # Ctrl-c to stop the program
+
+        # print the new maze after action is completed
         print_maze()
+        # end the program if the robot hit an obstacle or reached the goal
         if end_check is True:
             break
+        # continue program if robot has not hit obstacle or reached goal
+        else:
+            continue
 
 
 # Run the main function
--- rwa2_kim/robot.py
+++ rwa2_kim/robot.py
@@ -2,106 +2,202 @@
 This module contains the functions that control the robot's movement.
 """
 
-# from maze import maze, OBSTACLE, BOOM
-from maze import MAZE_SIZE
+# import maze size so that wall checks will work for any size maze
+# import ROBOT to use and update global variable as necessary in functions
+from maze import MAZE_SIZE, ROBOT
 
-# def obstacle_check():
-#     global robot_position
-#     if maze[robot_position[0]][robot_position[1]] is OBSTACLE:
-#         maze[robot_position[0]][robot_position[1]] = BOOM
-    
+# create function to move robot forward
 def move_forward(robot_position, robot_orientation):
-    """
-    TODO: Write documentation and implement this function
-    """
+    '''
+    This function moves the robot forward in its given orientation as long as there is not a wall in front of the robot.
+
+    Args:
+        robot_position (list[int]): The current position of the robot in the maze.
+        robot_orientation (str): The current orientation of the robot.
+
+    Returns:
+        robot_position(list[int]): The updated position of the robot in the maze.
+    '''
+    # pull row and col out of robot position in order to update values
     row, col = robot_position
+    # use global variable for maze size
     global MAZE_SIZE
+    # if robot orientation is up, subtract 1 from row to redefine robot position as one row up
     if robot_orientation == "up":
+        # only move the robot if it will not drive off the maze (into a wall)
         if row > 0:
             row -= 1
+    # if the robot orientation is down, add 1 to row to redefine robot posiiton as one row down
     elif robot_orientation == "down":
+        # only move the robot if it will not drive off the maze (into a wall)
         if row < MAZE_SIZE-1:
             row += 1
+    # if the robot orientation is left, subtract 1 from col to redefine robot position as one col to the left
     elif robot_orientation == "left":
+        # only move the robot if it will not drive off the maze (into a wall)
         if col > 0:
             col -= 1
+    # if the robot orientation is right, add 1 to col to redefine robot position as one col to the right
     elif robot_orientation == "right":
+        # only move the robot if it will not drive off the maze (into a wall)
         if col < MAZE_SIZE-1:
             col += 1
+    # print error message if for some reason all conditional statements above are skipped (this should not occur)
     else:
         print("Unknown error occured, try again.")
+    # redfine robot position with the updated row/col value
     robot_position = [row,col]
+    # return the new robot position  (note that this will be unchanged if the robot is facing a wall)
     return robot_position
 
+# create function to move robot backward
 def move_backward(robot_position, robot_orientation):
-    """
-    TODO: Write documentation and implement this function
-    """
+    '''
+    This function moves the robot backward in its given orientation as long as there is not a wall behind the robot.
+
+    Args:
+        robot_position (list[int]): The current position of the robot in the maze.
+        robot_orientation (str): The current orientation of the robot.
+
+    Returns:
+        robot_position (list[int]): The updated position of the robot in the maze.
+    '''
+    # pull row and col out of robot position in order to update values
     row, col = robot_position
+    # use global variable for maze size
+    global MAZE_SIZE
+    # if robot orientation is up, add 1 to row to redefine robot position as one row down
     if robot_orientation == "up":
+        # only move the robot if it will not drive off the maze (into a wall)
         if row < MAZE_SIZE-1:
             row += 1
+    # if the robot orientation is down, subtract 1 from row to redefine robot posiiton as one row up
     elif robot_orientation == "down":
+        # only move the robot if it will not drive off the maze (into a wall)
         if row > 0:
             row -= 1
+    # if the robot orientation is left, add 1 to col to redefine robot position as one col to the right
     elif robot_orientation == "left":
+        # only move the robot if it will not drive off the maze (into a wall)
         if col < MAZE_SIZE-1:
             col += 1
+    # if the robot orientation is right, subtract 1 from col to redefine robot position as one col to the left
     elif robot_orientation == "right":
+        # only move the robot if it will not drive off the maze (into a wall)
         if col > 0:
             col -= 1
+    # print error message if for some reason all conditional statements above are skipped (this should not occur)
     else:
         print("Unknown error occured, try again.")
+    # redfine robot position with the updated row/col value
     robot_position = [row,col]
+    # return the new robot position  (note that this will be unchanged if the robot is backed up to a wall)
     return robot_position
 
+# create function to turn robot left
 def turn_left(robot_orientation):
-    """
-    TODO: Write documentation and implement this function
-    """
+    '''
+    This function turns the robot left from its current orientation.
+
+    Args:
+        robot_orientation (str): The current orientation of the robot.
+
+    Returns:
+        new_orientation (str): The updated orientation of the robot.
+        ROBOT (str): The updated emoji showing robot orientation in the maze.
+    '''
+    # if current orientation is up, redefine orientation to left
     if robot_orientation == "up":
         new_orientation = "left"
+        # update robot emoji to show left arrow in maze
         ROBOT = "⏪"
+    # if current orientation is left, redefine orientation to down
     elif robot_orientation == "left":
         new_orientation = "down"
+        # update robot emoji to show down arrow in maze
         ROBOT = "⏬"
+    # if current orientation is down, redefine orientation to right
     elif robot_orientation == "down":
         new_orientation = "right"
+        # update robot emoji to show right arrow in maze
         ROBOT = "⏩"
+    # if current orientation is right, redefine orientation to up
     elif robot_orientation == "right":
         new_orientation = "up"
+        # update robot emoji to show up arrow in maze
         ROBOT = "⏫"
+    # print error message if for some reason all conditional statements above are skipped (this should not occur)
     else:
         print("Unknown error occured, try again.")
+    # return new orientation and robot emoji
     return new_orientation, ROBOT
 
+# create function to turn robot right
 def turn_right(robot_orientation):
-    """
-    TODO: Write documentation and implement this function
-    """
+    '''
+    This function turns the robot right from its current orientation.
+
+    Args:
+        robot_orientation (str): The current orientation of the robot.
+
+    Returns:
+        new_orientation (str): The updated orientation of the robot.
+        ROBOT (str): The updated emoji showing robot orientation in the maze.
+    '''
+    # if current orientation is up, redefine orientation to right
     if robot_orientation == "up":
         new_orientation = "right"
+        # update robot emoji to show right arrow in maze
         ROBOT = "⏩"
+    # if current orientation is right, redefine orientation to down
     elif robot_orientation == "right":
         new_orientation = "down"
+        # update robot emoji to show down arrow in maze
         ROBOT = "⏬"
+    # if current orientation is down, redefine orientation to left
     elif robot_orientation == "down":
         new_orientation = "left"
+        # update robot emoji to show left arrow in maze
         ROBOT = "⏪"
+    # if current orientation is left, redefine orientation to up
     elif robot_orientation == "left":
         new_orientation = "up"
+        # update robot emoji to show up arrow in maze
         ROBOT = "⏫"
+    # print error message if for some reason all conditional statements above are skipped (this should not occur)
     else:
         print("Unknown error occured, try again.")
+    # return new orientation and robot emoji
     return new_orientation, ROBOT
 
-def update_robot(action, ROBOT, robot_position, robot_orientation):
+# create function to move/update the robot depending on given action
+def update_robot(action, robot_position, robot_orientation):
+    '''
+    This function updates the robot position and orientation based on the action given by the user.
+
+    Args:
+        action (str): User input directing movement and turning of robot based on 'wasd' controls.
+        robot_position (list[int]): The current position of the robot in the maze.
+        robot_orientation (str): The current orientation of the robot.
+        
+    Returns:
+        ROBOT (str): Emoji representing robot's orientation after completing action.
+        robot_position(list[int]): The position of robot after completing action.
+        robot_orientation (str): The orientation of robot after completing action.
+    '''
+    # use global ROBOT to update, or output existing if no orientation change
+    global ROBOT
+    # if action input is w, move the robot forward
     if action == "w":
         robot_position = move_forward(robot_position, robot_orientation)
+    # if action input is s, move the robot backward
     elif action == "s":
         robot_position = move_backward(robot_position, robot_orientation)
+    # if action input is a, turn the robot left in place
     elif action == "a":
         robot_orientation, ROBOT = turn_left(robot_orientation)
+    # if action input is d, turn the robot right in place
     elif action == "d":
         robot_orientation, ROBOT = turn_right(robot_orientation)
+    # return updated robot parameters
     return ROBOT, robot_position, robot_orientation
