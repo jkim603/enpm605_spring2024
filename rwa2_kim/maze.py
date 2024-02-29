06ee347d9bfc488c4e94f04fc03deaed1153c3fe
jkim603 <113157667+jkim603@users.noreply.github.com>
2024-02-29 01:13:40 -0500

    Randomize goal position and robot start position

--- rwa2_kim/maze.py
+++ rwa2_kim/maze.py
@@ -2,6 +2,8 @@
 This module contains the maze and the robot's initial position, as well as functions to print the maze and move the robot.
 """
 
+# import random number generator to randomize start positions
+import random
 
 # Constants
 # Here are all the emojis: "💥", "🏁", "⏫", "⏩", "⏪", "⏬", "🚧"
@@ -21,16 +23,52 @@ MAZE_SIZE = 4
 maze = [[EMPTY] * MAZE_SIZE for _ in range(MAZE_SIZE)]
 # Rewrite the previous line using for loops
 
-
-# Define the robot's initial position
-robot_position = [2, 3]  # [row, column]
-robot_orientation = "up"
-
 # Define the obstacles' positions
 obstacle_positions = [[1, 1], [2, 2], [3, 3]]
 
+# Define the robot's initial position
+while True:
+    # randomize start position
+    x = random.choice(range(MAZE_SIZE))
+    y = random.choice(range(MAZE_SIZE))
+    # check if start position in obstacle
+    if [x,y] in obstacle_positions:
+        # rerandomize start if in obstacle
+        continue
+    # if not in obstacle, accept random start by breaking loop
+    else:
+        break
+# set robot position as the random x,y values
+robot_position = [x, y]  # [row, column]
+robot_orientation = random.choice(["up","down","left","right"])
+# set robot emoji to match randomized orientation
+if robot_orientation == "up":
+    ROBOT = "⏫"
+elif robot_orientation == "right":
+    ROBOT = "⏩"
+elif robot_orientation == "down":
+    ROBOT = "⏬"
+elif robot_orientation == "left":
+    ROBOT = "⏪"
+
 # Define the goal position
-goal_position = [3, 2]
+while True:
+    # randomize goal position
+    xx = random.choice(range(MAZE_SIZE))
+    yy = random.choice(range(MAZE_SIZE))
+    # check if random goal in obstacle
+    if [xx,yy] in obstacle_positions:
+        # rerandomize goal if in obstacle
+        continue
+    # check if goal is robot position
+    if [xx,yy] == robot_position:
+        # rerandomize goal if is robot start position
+        continue
+    # if not in obstacle or robot start, accept goal position by breaking loop
+    else:
+        break
+# set goal position as the random xx,yy values
+goal_position = [xx,yy]
 
 # Place obstacles, the robot, and the goal in the maze
 for obstacle in obstacle_positions:
